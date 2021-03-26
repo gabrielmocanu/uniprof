7bfd6a2f265579f08429bab6d8f17e8634a216d4
Viorel-Gabriel MOCANU (94548) <viorel.mocanu@stud.acs.upb.ro>
2021-03-26 02:11:43 +0200

    Adding a script to export /home/gabi/.cargo/bin:/home/gabi/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/gabi/.dotnet/tools:/usr/lib/jvm/jdk-15.0.2/bin/bin:/usr/local/go/bin:/home/gabi/go/bin and

---
+++ export.sh
@@ -0,0 +1,9 @@
+#!/bin/bash                                                                                                                                                                                  
+                                                                                                                                                                                             
+export PATH="/root/armtoolchain/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu/bin:/root/armtoolchain/qemu-4.0.0/bin:/root/armtoolchain/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu/
+ibexec/gcc/aarch64-linux-gnu/7.5.0:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/go/bin"                                                                                           
+                                                                                                                                                                                             
+export LD_LIBRARY_PATH="/root/gmocanu/xen-4.8/xen/tools/libs/ctrl:$LD_LIBRARY_PATH"                                                                                                          
+export LD_LIBRARY_PATH="/root/gmocanu/xen-4.8/xen/tools/libs/toollog:$LD_LIBRARY_PATH"                                                                                                       
+export LD_LIBRARY_PATH="/root/gmocanu/xen-4.8/xen/tools/libs/util:$LD_LIBRARY_PATH"                                                                                                          
+export LD_LIBRARY_PATH="/root/gmocanu/xen-4.8/xen/dist/install/usr/local/lib:$LD_LIBRARY_PATH"
